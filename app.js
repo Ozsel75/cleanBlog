0ec3d709a5aeee29e1a29901d2752a53696af38f
Oz SEL <ozsel75@hotmail.com>
2023-03-29 14:35:19 +0300

    ejs module added

--- app.js
+++ app.js
@@ -3,57 +3,64 @@
 // npm i nodemon
 // npm i mongoose
 
-const express = require("express")
-const ejs = require("ejs")
 
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const ejs = require("ejs");
+const methodOverride = require("method-override");
 
-const mongoose = require(mongoose)
+const app = express();
 
-const methodOverride = require(method-override)
+const Post = require("./modals/Post");
+const {
+  getAllPosts,
+  getPost,
+  createPost,
+  updatePost,
+  deletePost
+} = require("./controllers/postController");
+const {
+  aboutPage,
+  homePage,
+  addPage,
+  editPage
+} = require("./controllers/pageController");
 
-const Post = require("./models/Post")
-const postController = require("./controllers/postControllers")
-const pageController = require("./controllers/pageControllers")
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
 
-mongoose.set("strictQuery", false)
+// MIDDLEWARE
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
-mongoose
-    .connect("mogodb+srv")
-    .then(()=>{
-        console.log("Database connected")
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+// ROUTES
+app.get("/", getAllPosts); // home page
+app.get("/post/:id", getPost); // post detail page
+app.post("/add", createPost); // add new post
+app.put("/post/:id", updatePost); // update post
+app.delete("/post/:id", deletePost); // delete post
+app.get("/home", homePage); // home page
+app.get("/about", aboutPage); // about page
+app.get("/add", addPage); // add post page
+app.get("/post/edit/:id", editPage); // edit post page
 
-app.set("view engine", "ejs")
+mongoose.set("strictQuery", false);
+mongoose.connect(
+  "mongodb+srv:/...",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  err => {
+    if (err) throw err;
+    console.log("Connected to MongoDB");
 
-app.use(express.static("public"))
-
-app.use(express.urlencoded({extends: true}))
-app.use(express.json())
-
-app.use(
-    methodOverride("_method", {
-        methods: ["POST", "GET"]
-    })
-)
-
-app.get("/", postController.getAllPost)
-app.get("/about", pageController.getAboutPage)
-app.get("/addpost", pageController.getAddPage)
-app.post("/add", postController.addPost)
-
-app.get("/myposts/:id", pageController.getPostPage)
-app.delete("/post/:id", postController.deletePost)
-app.post("/post/edit/:id", pageController.getEditPage)
-app.put("/post/update/:id", postController.editPost)
-
-const port = 3000;
-
-app.listen(port, ()=>{
-    console.log("server started")
-})
-
-
+    const port = 3000;
+    app.listen(port, () => {
+      console.log(`server started on port: ${PORT}`);
+    });
+  }
+);
